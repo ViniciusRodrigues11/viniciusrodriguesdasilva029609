e5f6a1f5512c98632a4d7751f2c9650d352244cd
viniciusrodrigues <vrsstudy@gmail.com>
2026-01-18 20:26:55 -0400

    fix: increase z-index of Modal component for improved overlay visibility

--- src/presentation/components/modal/modal.tsx
+++ src/presentation/components/modal/modal.tsx
@@ -47,7 +47,7 @@ export function Modal({
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#000000ba]">
+    <div className="fixed inset-0 z-100 flex items-center justify-center bg-[#000000ba]">
       <div
         className="fixed inset-0"
         onClick={handleBackdropClick}
